56f32e5d8d11dbfdfc151da5cc352481001b58ee
Parag <paragnaik2021.git@gmail.com>
2022-02-15 10:43:57 +0530

    24.GFG.Swap node in pairs using recursion
    24.GFG.Swap node in pairs using recursion

--- Linked_List/24-GFG.Swap Nodes in Pairs.py
+++ Linked_List/24-GFG.Swap Nodes in Pairs.py
@@ -18,3 +18,29 @@ class Solution:
             curr=nextPair
             
         return dummy.next
+
+
+#################--Recurssion--###########################
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy=ListNode(0,head)
+        prev,curr=dummy,head
+        
+        def swapNode(prev,curr):
+            if curr==None or curr.next==None:
+                return dummy.next
+            
+            nextPair=curr.next.next
+            second=curr.next
+            
+            #swapping
+            second.next=curr
+            curr.next=nextPair
+            prev.next=second
+            #recurrsion call
+            swapNode(curr,nextPair)
+        
+        swapNode(prev,curr)
+        
+        return dummy.next
