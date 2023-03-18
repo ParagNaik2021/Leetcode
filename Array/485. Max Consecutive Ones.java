1c4b090e0e4fba63983d532f4f655aa5a5b8ee71
Parag18081994 <paragnaik2021.git@gmail.com>
2023-03-18 21:27:21 +0530

    485. Max Consecutive Ones
    485. Max Consecutive Ones

---
+++ Array/485. Max Consecutive Ones.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int maxi=0;
+        int counter=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                counter++;
+                maxi=Math.max(counter,maxi);
+                
+            }
+            else
+            {
+                counter=0;
+            }
+
+        }
+        return maxi;
+        
+    }
+}
