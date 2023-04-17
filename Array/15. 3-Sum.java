7dcff30093792cb3a5bd677f2f389737fa6009e9
Parag18081994 <paragnaik2021.git@gmail.com>
2023-04-17 12:02:39 +0530

    15. 3 Sum
    15. 3 Sum

---
+++ Array/15. 3-Sum.java
@@ -0,0 +1,40 @@
+**********************************************************************************************8
+TC-->O(N)*O(M)
+SC-->O(M)
+
+class Solution {
+   public List < List < Integer >> threeSum(int[] nums) {
+      Arrays.sort(nums);
+      List < List < Integer >> res = new LinkedList < > ();
+
+      //We end the loop at len-2 because last ele is marked as high
+      for (int i = 0; i < nums.length - 2; i++) {
+         if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+            int lo = i + 1;
+            int hi = nums.length - 1;
+            int sum = 0 - nums[i];
+
+            while (lo < hi) {
+               if (nums[lo] + nums[hi] == sum) {
+                  res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+
+                  while (lo < hi && nums[lo] == nums[lo + 1])
+                     lo++;
+
+                  while (lo < hi && nums[hi] == nums[hi - 1])
+                     hi--;
+
+                  lo++;
+                  hi--;
+
+               } else if (nums[lo] + nums[hi] < sum)
+                  lo++;
+
+               else hi--;
+            }
+            9
+         }
+      }
+      return res;
+   }
+}
