604be43c43ad1a4a8d32002be26235c66b6ecc81
Parag18081994 <paragnaik2021.git@gmail.com>
2023-04-20 11:29:19 +0530

    18. 4 Sum
    18. 4 Sum-
    1. Brute Force
    2. Better Solution
    3. Optimal Solution

--- Array/18. 4 Sum.java
+++ Array/18. 4 Sum.java
@@ -1,3 +1,91 @@
+*************************************Brute Force********************************************************************
+287 / 292 testcases passed
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+          List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    for (int h = k + 1; h < nums.length; h++) {
+                        if (nums[i] + nums[j] + nums[k] + nums[h] == target) {
+                            List temp = new ArrayList<>();
+                            temp.add(nums[i]);
+                            temp.add(nums[j]);
+                            temp.add(nums[k]);
+                            temp.add(nums[h]);
+                          Collections.sort(temp);
+                            result.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+        LinkedHashSet<List<Integer>> hashSet = new LinkedHashSet<>(result);
+        List<List<Integer>> ans = new ArrayList<>(hashSet);
+        return ans;
+    }
+}
+
+
+**************************************Better Solution****************************************************************
+
+290 / 292 testcases passed
+
+class Solution {
+   public List < List < Integer >> fourSum(int[] nums, int target) {
+      List < List < Integer >> result = new ArrayList < > ();
+
+      int n = nums.length;
+      if (n < 4) {
+         return result;
+      }
+
+      int sum1 = nums[0] + nums[1] + nums[2] + nums[3];
+      if (n == 4 && sum1 == target) {
+         List temp = new ArrayList < > ();
+         for (int i = 0; i < n; i++) {
+            temp.add(nums[i]);
+         }
+         result.add(temp);
+         return result;
+      }
+
+      if (nums.length == 4 && sum1 == target) {
+         System.out.println(sum1);
+         return result;
+      }
+
+      for (int i = 0; i < nums.length; i++) {
+         for (int j = i + 1; j < nums.length; j++) {
+            HashSet hs = new HashSet < > ();
+            for (int k = j + 1; k < nums.length; k++) {
+               int sum = nums[i] + nums[j];
+               sum += nums[k];
+               int fourth = target - (sum);
+               if (hs.contains(fourth)) {
+                  List temp = new ArrayList < > ();
+                  temp.add(nums[i]);
+                  temp.add(nums[j]);
+                  temp.add(nums[k]);
+                  temp.add(fourth);
+                  Collections.sort(temp);
+                  result.add(temp);
+               }
+               hs.add(nums[k]);
+            }
+         }
+      }
+
+      LinkedHashSet < List < Integer >> hashSet = new LinkedHashSet < > (result);
+      List < List < Integer >> ans = new ArrayList < > (hashSet);
+      return ans;
+
+   }
+}
+
+
 
 
 **************************************Optimized Solution**************************************************************
