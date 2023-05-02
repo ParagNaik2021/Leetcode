e58bf286841389d65facfb3f6ed8cbfa7daf2aba
Parag18081994 <paragnaik2021.git@gmail.com>
2023-05-02 15:11:06 +0530

    Sorting GFG
    Sorting GFG

---
+++ Sorting/GFG. Insertion Sort.java
@@ -0,0 +1,24 @@
+********************************************** Insertion Sort ************************************************
+
+class Solution
+{
+  static void insert(int arr[],int i)
+  {
+       // Your code here
+  }
+  //Function to sort the array using insertion sort algorithm.
+  public void insertionSort(int arr[], int n)
+  {
+      //code here
+       for (int i = 0; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+  }
+       }
+  }
+  
+}
