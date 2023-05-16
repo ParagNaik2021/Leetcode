8673f0a0794f1e2cf4a71a603941934b08b2af74
Parag18081994 <paragnaik2021.git@gmail.com>
2023-05-16 18:01:27 +0530

    Inorder Traversal-Iterative approach
    Inorder Traversal-Iterative approach

--- Binary Search Tree/Java/94. Binary Tree Inorder Traversal.java
+++ Binary Search Tree/Java/94. Binary Tree Inorder Traversal.java
@@ -16,3 +16,31 @@ class Solution {
     }
 
 }
+
+
+*******************************Inorder Traversal-->Iterative Approach*****************************************
+
+class Solution {
+   public List < Integer > inorderTraversal(TreeNode root) {
+
+      List < Integer > inorder = new ArrayList < > ();
+      Stack < TreeNode > st = new Stack < > ();
+
+      while (!st.empty() || root != null) {
+         // if the current node exists, push it into the stack (defer it)
+         // and move to its left child
+         if (root != null) {
+            st.push(root);
+            root = root.left;
+         } else {
+            // otherwise, if the current node is null, pop an element from
+            // the stack, print it, and finally set the current node to its
+            // right child
+            root = st.pop();
+            inorder.add(root.val);
+            root = root.right;
+         }
+      }
+      return inorder;
+   }
+}
