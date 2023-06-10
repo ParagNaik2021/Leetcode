3059f8747c285c1c4e7ca1acfe2c2e6163b9aa5d
Parag18081994 <paragnaik2021.git@gmail.com>
2023-06-10 13:06:18 +0530

    Updated Stickler thief code for memoization and tabulation approach
    Updated Stickler thief code for memoization and tabulation approach

--- Dynamic Programming/GFG. Stickler Thief(Maximum sum of non-adjacent elements).java
+++ Dynamic Programming/GFG. Stickler Thief(Maximum sum of non-adjacent elements).java
@@ -19,45 +19,68 @@ class Solution {
 }
 
 ****************************Memoization approach***************************************************
-class Solution {
-   public int f(int index, int[] arr) {
-      if (index == 0) return arr[index];
-      if (index < 0) return 0;
 
-      int picked = arr[index] + f(index - 2, arr);
-      int notpicked = 0 + f(index - 1, arr);
 
-      return Math.max(picked, notpicked);
-   }
-   //Function to find the maximum money the thief can get.
-   public int FindMaxSum(int arr[], int n) {
-      // Your code here
-
-      return f(n - 1, arr);
-   }
+class Solution
+{
+    public int f(int index,int[] arr,int[] dp)
+    {
+        if(index==0) return arr[index];
+        if(index<0) return 0;
+        
+        if(dp[index]!=-1)
+            return dp[index];
+            
+        int picked=arr[index]+f(index-2,arr,dp);
+        int notpicked=0+f(index-1,arr,dp);
+        
+        return dp[index]=Math.max(picked,notpicked);
+        
+        
+        
+    }
+    //Function to find the maximum money the thief can get.
+    public int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        
+        return f(n-1,arr,dp);
+    }
+    
 }
 
 
 ***************************************Tabulation approach****************************************
 
-class Solution {
-   public int f(int index, int[] arr) {
-      if (index == 0) return arr[index];
-      if (index < 0) return 0;
-
-      int picked = arr[index] + f(index - 2, arr);
-      int notpicked = 0 + f(index - 1, arr);
-
-      return Math.max(picked, notpicked);
-   }
-   //Function to find the maximum money the thief can get.
-   public int FindMaxSum(int arr[], int n) {
-      // Your code here
-
-      return f(n - 1, arr);
-   }
+class Solution
+{
+    public int f(int index,int[] arr,int[] dp)
+    {
+        dp[0]=arr[0];
+        int neg=0;
+        for(int i=1;i<=index;i++)
+        {
+            int picked=arr[i];
+            if(i>1)
+            {
+                picked+=dp[i-2];
+            }
+            int notpicked=0+dp[i-1];
+            dp[i]=Math.max(picked,notpicked);
+        }
+        return dp[index];
+        
+    }
+   
+    public int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int[] dp=new int[n];
+        return f(n-1,arr,dp);
+    }
 }
-
 
 
 ***************************************space optimazation approach****************************************
