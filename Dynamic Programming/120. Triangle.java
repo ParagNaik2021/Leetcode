8de7434d10ff153fa139efaed5897471ce375b08
Parag18081994 <paragnaik2021.git@gmail.com>
2023-06-19 21:25:15 +0530

    Updated memoization approach
    Updated memoization approach

--- Dynamic Programming/120. Triangle.java
+++ Dynamic Programming/120. Triangle.java
@@ -41,7 +41,32 @@ public class Solution {
 
 ****************************Memoization approach***************************************************
 //Memoization
+import java.util.* ;
+import java.io.*; 
+public class Solution {
 
+  public static int f(int i,int j,int n,int[][] triangle,int[][] dp)
+    {
+        if(i==n-1) return triangle[n-1][j];
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+       int down=triangle[i][j]+f(i+1,j,n,triangle,dp);
+       int dia=triangle[i][j]+f(i+1,j+1,n,triangle,dp);
+
+        return dp[i][j]=Math.min(down, dia);
+    }
+    public static int minimumPathSum(int[][] triangle, int n) {
+        // Write your code here.
+   
+      int m= triangle.length;
+        int n1= triangle[0].length;
+        int[][] dp=new int[m][m];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        return f(0,0,n,triangle,dp);
+    }
+}
 
 
 ***************************************Tabulation approach****************************************
