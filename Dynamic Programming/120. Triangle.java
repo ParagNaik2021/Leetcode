d53ca93e2212b298214a6960b9ba49f43cbae230
Parag18081994 <paragnaik2021.git@gmail.com>
2023-06-19 20:45:02 +0530

    120. Triangle (Recurrsion)
    120. Triangle (Recurrsion)

--- Dynamic Programming/120. Triangle.java
+++ Dynamic Programming/120. Triangle.java
@@ -1,6 +1,43 @@
 **********************************Recurssion- Unique Path but time limit exceed *****************************************
-//Recurrssion
+//Recurrssion-Leetcode
+class Solution {
 
+  public static int f(int i, int j, int n, List < List < Integer >> triangle) {
+    if (i == n - 1) return triangle.get(n - 1).get(j);
+
+    int down = triangle.get(i).get(j) + f(i + 1, j, n, triangle);
+    int dia = triangle.get(i).get(j) + f(i + 1, j + 1, n, triangle);
+
+    return Math.min(down, dia);
+  }
+
+  public int minimumTotal(List < List < Integer >> triangle) {
+    int n = triangle.size();
+    return f(0, 0, n, triangle);
+  }
+}
+
+
+//Recurrssion-Coding Ninja
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+    public static int f(int i,int j,int n,int[][] triangle)
+    {
+        if(i==n-1) return triangle[n-1][j];
+
+        int down=triangle[i][j]+f(i+1,j,n,triangle);
+        int dia=triangle[i][j]+f(i+1,j+1,n,triangle);
+
+        return Math.min(down, dia);
+    }
+    public static int minimumPathSum(int[][] triangle, int n) {
+        // Write your code here.
+   
+        return f(0,0,n,triangle);
+    }
+}
 
 ****************************Memoization approach***************************************************
 //Memoization
