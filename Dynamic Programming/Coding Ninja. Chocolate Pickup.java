c4a0a61b80b354edfd06841b77310e09e9ea00c0
Parag18081994 <paragnaik2021.git@gmail.com>
2023-06-22 12:57:08 +0530

    Chocolate pickup -->space optimized approach
    Chocolate pickup -->space optimized approach

--- Dynamic Programming/Coding Ninja. Chocolate Pickup.java
+++ Dynamic Programming/Coding Ninja. Chocolate Pickup.java
@@ -140,3 +140,59 @@ public class Solution {
 
 ***************************************Space optimization approach****************************************
 //Space optimization
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+	public static int maximumChocolates(int n, int m, int[][] grid) {
+		// Write your code here.
+		int front[][]=new int[m][m];
+		int curr[][]=new int[m][m];
+		//base case
+		for(int j1=0;j1<m;j1++)
+		{
+			for(int j2=0;j2<m;j2++)
+			{
+				if(j1==j2) 
+				front[j1][j2]=grid[n-1][j1];
+				else
+				 front[j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+			}
+		}
+		
+		for(int i=n-2;i>=0;i--)
+		{
+			for(int j1=0;j1<m;j1++)
+			{
+				for(int j2=0;j2<m;j2++)
+				{
+					int maxi=(int) Math.pow(-10,9);
+		for(int dj1=-1;dj1<=+1;dj1++)
+		{
+			for(int dj2=-1;dj2<=+1;dj2++)
+			{
+				int value=0;
+				if(j1==j2) value=grid[i][j1];
+				else value=grid[i][j1]+grid[i][j2];
+				if(j1+dj1>=0 && j1+dj1<m && j2+dj2>=0 && j2+dj2<m) value+=front[j1+dj1][j2+dj2];
+				else value+=(int) Math.pow(-10,9);
+				maxi=Math.max(maxi,value);
+
+			}
+		}
+
+		 curr[j1][j2]=maxi;
+				}
+			}
+			 for (int a = 0; a < m; a++) {
+        front[a] = (int[])(curr[a].clone());
+      }
+		}
+
+		
+
+return front[0][m-1];
+		
+
+	}
+}
