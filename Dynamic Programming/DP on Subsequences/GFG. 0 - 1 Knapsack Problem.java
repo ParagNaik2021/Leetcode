569d19d32da54b035a5412dc7e9f65526de2ad81
Parag18081994 <paragnaik2021.git@gmail.com>
2023-06-28 11:35:11 +0530

    Tabulation, space optimization added
    Tabulation, space optimization added

--- Dynamic Programming/DP on Subsequences/GFG. 0 - 1 Knapsack Problem.java
+++ Dynamic Programming/DP on Subsequences/GFG. 0 - 1 Knapsack Problem.java
@@ -64,8 +64,36 @@ class Solution
 
 ***************************************Tabulation approach****************************************
 //Tabulation
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         // your code here 
+        
+         int[][] dp=new int[n][W+1];
+		//base case
+		for(int i=wt[0];i<=W;i++) dp[0][i]=val[0];
+		
+		for(int ind=1;ind<n;ind++)
+		{
+		    for(int cap=0;cap<=W;cap++)
+		    {
+		        int notTaken=0+dp[ind-1][cap];
+		        int taken=(int) Math.pow(-10,9);
+		        if(wt[ind]<=cap) taken=val[ind]+dp[ind-1][cap-wt[ind]];
+		        
+		        dp[ind][cap]=Math.max(notTaken,taken);
+		    }
+		}
+	
+	return dp[n-1][W];
+        
+    } 
+}
 
-   
+
+
 
 
 ***************************************Space optimization approach****************************************
