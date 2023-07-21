f0cb3157fb25e5f6506887dbf3e833ba707f15c9
Parag18081994 <paragnaik2021.git@gmail.com>
2023-07-21 10:53:33 +0530

    309. Best Time to Buy and Sell Stock with Cooldown
    309. Best Time to Buy and Sell Stock with Cooldown

---
+++ Dynamic Programming/DP on Stocks/309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -0,0 +1,92 @@
+**********************************Recurssion- Unique Path but time limit exceed *****************************************
+//Recurrssion-Leetcode--->TLE
+//Recurssion
+class Solution {
+  public int f(int ind, int buy, int[] prices, int n) {
+    //base case
+    if (ind >= prices.length) return 0;
+    int profit = 0;
+    if (buy == 1) {
+      profit = Math.max((-prices[ind] + f(ind + 1, 0, prices, n)), (0 + f(ind + 1, 1, prices, n)));
+    } else {
+      profit = Math.max((prices[ind] + f(ind + 2, 1, prices, n)), (0 + f(ind + 1, 0, prices, n)));
+    }
+    return profit;
+  }
+  public int maxProfit(int[] prices) {
+    int n = prices.length;
+    int buy = 1;
+    return f(0, buy, prices, n);
+  }
+}
+
+****************************Memoization approach***************************************************
+//Memoization
+class Solution {
+  public int f(int ind, int buy, int[] prices, int n, int[][] dp) {
+    //base case
+    if (ind >= prices.length) return 0;
+    int profit = 0;
+    if (dp[ind][buy] != -1) return dp[ind][buy];
+    if (buy == 1) {
+      profit = Math.max((-prices[ind] + f(ind + 1, 0, prices, n, dp)), (0 + f(ind + 1, 1, prices, n, dp)));
+    } else {
+      profit = Math.max((prices[ind] + f(ind + 2, 1, prices, n, dp)), (0 + f(ind + 1, 0, prices, n, dp)));
+    }
+    return dp[ind][buy] = profit;
+  }
+  public int maxProfit(int[] prices) {
+    int n = prices.length;
+    int buy = 1;
+    int[][] dp = new int[n][2];
+    for (int[] row: dp) Arrays.fill(row, -1);
+
+    return f(0, buy, prices, n, dp);
+  }
+}
+
+***************************************Tabulation approach****************************************
+//Tabulation
+
+class Solution {
+
+  public int maxProfit(int[] prices) {
+    int n = prices.length;
+    int[][] dp = new int[n + 2][2];
+    for (int ind = n - 1; ind >= 0; ind--) {
+      for (int buy = 0; buy <= 1; buy++) {
+        if (buy == 1) {
+          dp[ind][buy] = Math.max((-prices[ind] + dp[ind + 1][0]), (0 + dp[ind + 1][1]));
+        } else {
+          dp[ind][buy] = Math.max((prices[ind] + dp[ind + 2][1]), (0 + dp[ind + 1][0]));
+        }
+      }
+    }
+
+    return dp[0][1];
+  }
+}
+   
+
+
+***************************************Space optimization approach****************************************
+//Space optimization---No space optimization because of ind+2 as we need to carry 3 variables
+
+
+***************************************for loop optimization approach****************************************
+//remove buy-for loop because it is redundant
+class Solution {
+
+  public int maxProfit(int[] prices) {
+    int n = prices.length;
+    int[][] dp = new int[n + 2][2];
+    for (int ind = n - 1; ind >= 0; ind--) {
+
+      dp[ind][1] = Math.max((-prices[ind] + dp[ind + 1][0]), (0 + dp[ind + 1][1]));
+      dp[ind][0] = Math.max((prices[ind] + dp[ind + 2][1]), (0 + dp[ind + 1][0]));
+
+    }
+
+    return dp[0][1];
+  }
+}
