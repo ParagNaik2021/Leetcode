322059618a7649a6ae03b238b91d1645a06c75b7
Parag <paragnaik2021.git@gmail.com>
2023-08-27 14:51:58 -0700

    updated 27 Aug 2023
    updated 27 Aug 2023

--- Array/26. Remove Duplicates from Sorted Array.java
+++ Array/26. Remove Duplicates from Sorted Array.java
@@ -12,3 +12,24 @@ class Solution {
         return i+1;
     }
 }
+
+#####################################Coding ninja solution##############################################
+Note- Logic is same but here ArrayList is used and dont use add Function of Al use Set function of AL
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static int removeDuplicates(ArrayList<Integer> arr,int n) {
+        // Write your code here.
+        int i=0;
+        for(int j=1;j<n;j++)
+        {
+            if(!arr.get(j).equals(arr.get(i))){
+                arr.set(i+1,arr.get(j));
+                i++;
+            }
+            
+        }
+        return i+1;
+    }
+}
