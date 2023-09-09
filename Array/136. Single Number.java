e5764268becfa2544af5803ac6fc16e38b5a4c02
Parag <paragnaik2021.git@gmail.com>
2023-09-09 12:02:07 -0700

    136. Single Number -update
    Update in code documentation

--- Array/136. Single Number.java
+++ Array/136. Single Number.java
@@ -1,5 +1,25 @@
 
-//Optimal solution
+####################################Brute approach#########################################################
+class Solution {
+    public int singleNumber(int[] nums) {
+        int count=0;
+        int n=nums.length;
+        if(n==1) return nums[0];
+        for(int i=0;i<n;i++){
+            count=0;
+            int val=nums[i];
+            for(int j=0;j<n;j++){
+                if(val==nums[j]) count++;
+            }
+            if(count==1) return val;
+        }
+    return 0;
+    }
+}
+
+
+
+####################################Optimal approach#########################################################
 class Solution {
     public int singleNumber(int[] nums) {
        int XOR1=0;
