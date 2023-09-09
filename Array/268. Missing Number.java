5286ffd80c1dcb9433fd369dae1876ed1ac63375
Parag <paragnaik2021.git@gmail.com>
2023-09-08 17:26:08 -0700

    updated 268. Missing number
    updated 268. Missing number

--- Array/268. Missing Number.java
+++ Array/268. Missing Number.java
@@ -1,3 +1,5 @@
+
+############################################Optimal Solution-XOR approach###############################################
 class Solution {
     public int missingNumber(int[] nums) {
       //Most optimal soln
@@ -12,7 +14,10 @@ class Solution {
         // XOR1=XOR1^N;//0 <= nums[i] <= n thats why no need of this line
          return XOR1^XOR2;
 
-        /*
+############################################Optimal Solution-Formula approach###############################################
+
+class Solution {
+    public int missingNumber(int[] nums) {     
         //Optimal solution
         //    int N=nums.length;
         //    int summation=(N)*(N+1)/2;// this will create a problem as int cannot store 10^10 so XOR is better approach
@@ -21,5 +26,25 @@ class Solution {
                 sum+=nums[i];
             return summation-sum;
         */
+    }
+}
+
+
+############################################Better Solution###############################################
+
+class Solution {
+    int missingNumber(int array[], int n) {
+        // Your Code Here
+        
+        int[] hs=new int[n+1];
+        for(int i=0;i<n-1;i++){
+            hs[array[i]]++;
+        }    
+            for(int i=1;i<=n;i++){
+                if(hs[i]==0) return i;
+            }
+        
+        return -1;
+        
     }
 }
